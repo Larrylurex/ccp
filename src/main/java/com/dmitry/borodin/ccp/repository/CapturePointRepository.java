837e1550de92529799dccf7cb501f14d0dbb297e
Dmitry Borodin <dmitry.borodin90@gmail.com>
2018-07-10 12:19:22 +0300

    Added swagger Downgrade spring boot version for swagger compatibility Added linux script for service management

--- src/main/java/com/dmitry/borodin/ccp/repository/CapturePointRepository.java
+++ src/main/java/com/dmitry/borodin/ccp/repository/CapturePointRepository.java
@@ -1,9 +1,9 @@
 package com.dmitry.borodin.ccp.repository;
 
 import com.dmitry.borodin.ccp.entity.Interaction;
+import io.swagger.annotations.Api;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@Api("Interaction")
 public interface CapturePointRepository extends JpaRepository<Interaction, Integer> {
 }
